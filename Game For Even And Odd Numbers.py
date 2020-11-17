2af5615816990a1c7bdf2e6bbcf5d0039f227317
yasersarhan <sarhan.whitewhale@gmail.com>
2020-11-17 11:46:25 +0200

    Create Class Number and initlize constructor print welcome message and choice for users

---
+++ Game For Even And Odd Numbers.py
@@ -0,0 +1,8 @@
+class Numbers:
+    def __init__(self):
+        print('Welcome in our game even and odd numbers')
+        print('''
+               Press The Game Number:
+                  1-For Even Numbers.
+                  2-For Odd Numbers.
+            ''')
