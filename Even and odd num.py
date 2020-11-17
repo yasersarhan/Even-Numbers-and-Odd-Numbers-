4f7b28745ebfa61046cd9cd3e069712204c13d6b
yasersarhan <sarhan.whitewhale@gmail.com>
2020-11-17 10:46:38 +0200

    Create forloop and neasted if

--- Even and odd num.py
+++ Even and odd num.py
@@ -2,4 +2,11 @@ def numbers(args,*vartuple):
     x = args
     y = list(vartuple)
     y.append(x)
-     
+
+    for num in y:
+        if num % 2 == 0:
+            print('This is an even number')
+        else:
+            print('This is an odd number')
+    
+numbers(23,34,87)
